a82fa667afbb6f3ca19286d91dcdd4ef922cf4f2
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-14 20:32:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,11 +13,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
- function myFunction() {
-    y = cat;
-    console.log(dog);
-  }
-
+ return animal
 }
 
 function add2(n) {
