709961bcf6dea70e675c99e2aa9dc42d1260cbd1
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-14 20:00:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  return animal
+  
 }
 
 function add2(n) {
